765b20cb1cb15e2a09ad1a47cba83d769f20d7d8
Matej Capkovic <mcapkovic@gmail.com>
2023-07-29 09:14:57 +0200

    add lyrics

--- src/js/musicDashboard.js
+++ src/js/musicDashboard.js
@@ -2,14 +2,6 @@ const songs = songsData.filter(
   (song) => song.artist != null && song.artist != ""
 );
 
-function getTileTitleElement(element) {
-  return element.querySelector(".tile__title");
-}
-
-function getTileImageElement(element) {
-  return element.querySelector(".tile__image");
-}
-
 const tileElement = document.querySelector(".tile");
 const containerElement = document.querySelector(".container");
 
@@ -17,11 +9,23 @@ const tiles = songs
   .map((song, index) => {
     const newTileElement =
       index === 0 ? tileElement : tileElement.cloneNode(true);
-    const titleElement = getTileTitleElement(newTileElement);
-    const imageElement = getTileImageElement(newTileElement);
+    const titleElement = newTileElement.querySelector(".tile__title");
+    const imageElement = newTileElement.querySelector(
+      ".tile__song-link__image"
+    );
+    const linkElement = newTileElement.querySelector(".tile__song-link");
+    const lyricsElement = newTileElement.querySelector(".tile__lyrics");
 
     titleElement.textContent = `${song.artist} - ${song.song_name}`;
     imageElement.src = song.song_img;
+    linkElement.href = song.song_url;
+
+    const lyrics = song.song_lyrics
+      ?.split("\n")
+      .filter((_, index) => index < 10)
+      .join("\n");
+
+    lyricsElement.textContent = lyrics ? `${lyrics}\n...` : "";
 
     return newTileElement;
   })
